0d46401d565b0811f61c8c42d7ca3aed9bc8db97
1582421598 <1582421598@qq.com>
2022-01-28 15:09:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-sudo mkdir -p /app/docker/my-tengine
 mkdir my-tengine
 cd my-tengine
 wget http://tengine.taobao.org/download/tengine-2.3.3.tar.gz
@@ -6,8 +5,7 @@ wget https://github.com/aperezdc/ngx-fancyindex/releases/download/v0.5.2/ngx-fan
 wget https://github.com/FRiCKLE/ngx_cache_purge/archive/refs/tags/2.3.tar.gz
 wget https://github.com/hongzhidao/nginx-upload-module/archive/refs/heads/master.zip
 tar -xzvf tengine-2.3.3.tar.gz
-xz -d ngx-fancyindex-0.5.2.tar.xz
-tar -xvf ngx-fancyindex-0.5.2.tar
+tar -axvf ngx-fancyindex-0.5.2.tar.xz
 tar -xzvf 2.3.tar.gz
 unzip master.zip
 sudo docker build -t my-tengine:latest .
