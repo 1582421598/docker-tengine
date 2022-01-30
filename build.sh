3033c6c8f5e776da3a0f08c787f92ea4dceb6bcb
1582421598 <1582421598@qq.com>
2022-01-30 23:28:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir my-tengine
 cd my-tengine
-wget http://tengine.taobao.org/download/tengine-2.3.3.tar.gz
+wget https://github.com/1582421598/tengine/archive/refs/tags/tengine-2.3.3.tar.gz
 wget https://github.com/aperezdc/ngx-fancyindex/releases/download/v0.5.2/ngx-fancyindex-0.5.2.tar.xz
 wget https://github.com/FRiCKLE/ngx_cache_purge/archive/refs/tags/2.3.tar.gz
 wget https://github.com/hongzhidao/nginx-upload-module/archive/refs/heads/master.zip
