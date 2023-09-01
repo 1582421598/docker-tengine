fe1115a77dd022d67c0511c6332a3dd2e398459f
1582421598 <26507452+1582421598@users.noreply.github.com>
2023-09-02 06:30:45 +0800

    Remove QUIC

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ wget https://github.com/aperezdc/ngx-fancyindex/archive/refs/heads/master.zip -O
 wget https://github.com/FRiCKLE/ngx_cache_purge/archive/refs/tags/2.3.tar.gz
 unzip tengine.zip
 unzip ngx-fancyindex.zip
-tar -axvf 2.3.tar.gz
+tar -xf 2.3.tar.gz
 sudo docker build -f ../Dockerfile -t my-tengine:latest .
 sudo docker run -p 80:80 -p 443:443 --name Tengine -d my-tengine:latest
