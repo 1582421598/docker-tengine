dd714457a1ff99381cbebd0b567d2a16b6354c86
1582421598 <26507452+1582421598@users.noreply.github.com>
2023-09-02 05:58:34 +0800

    Enhancing the Generality of Automation Scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 mkdir my-tengine
 cd my-tengine
-wget https://github.com/1582421598/tengine/archive/refs/tags/2.4.0.tar.gz -O tengine-2.4.0.tar.gz
-wget https://github.com/aperezdc/ngx-fancyindex/releases/download/v0.5.2/ngx-fancyindex-0.5.2.tar.xz
+wget https://github.com/1582421598/tengine/archive/refs/heads/master.zip -O tengine.zip
+wget https://github.com/aperezdc/ngx-fancyindex/archive/refs/heads/master.zip -O ngx-fancyindex.zip
 wget https://github.com/FRiCKLE/ngx_cache_purge/archive/refs/tags/2.3.tar.gz
-tar -axvf tengine-2.4.0.tar.gz
-tar -axvf ngx-fancyindex-0.5.2.tar.xz
+unzip tengine.zip
+unzip ngx-fancyindex.zip
 tar -axvf 2.3.tar.gz
 sudo docker build -f ../Dockerfile -t my-tengine:latest .
 sudo docker run -p 80:80 -p 443:443 --name Tengine -d my-tengine:latest
